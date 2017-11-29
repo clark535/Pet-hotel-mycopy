5a35cdab9e25a9e9c914d83ec86ffda2efa03782
David Clark <primecomp@Davids-MacBook-Pro.local>
2017-11-28 19:48:58 -0600

    created database and three tables

---
+++ database.sql
@@ -0,0 +1,21 @@
+--database name is: sigma
+
+CREATE TABLE owners (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(80),
+last_name VARCHAR(80)
+);
+
+CREATE TABLE pets (
+id SERIAL PRIMARY KEY,
+name VARCHAR(80),
+breed VARCHAR(80),
+color VARCHAR(80),
+is_checked_in BOOLEAN
+);
+
+CREATE TABLE visits (
+id SERIAL PRIMARY KEY,
+check_in_date INT,
+check_out_date INT
+);
